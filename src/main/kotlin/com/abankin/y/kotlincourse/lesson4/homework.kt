da286750dfad5f02388a5563f4001e21345ca92b
abankin <y.abankin@yandex.ru>
2024-09-15 08:34:33 +0300

    lesson4

---
+++ src/main/kotlin/com/abankin/y/kotlincourse/lesson4/homework.kt
@@ -0,0 +1,45 @@
+package com.abankin.y.kotlincourse.lesson4
+
+var var1: Int = 42
+var var2: Long = 98765432123456789L
+var var3: Float = 23.45f
+var var4: Double = 0.123456789
+var var5: String = "Kotlin & Java"
+var var6: Boolean = false
+var var7: Char = 'c'
+var var8: Int = 500
+var var9: Long = 4294967296L
+var var10: Float = 18.0f
+var var11: Double = -0.001
+var var12: String = "OpenAI"
+“true” // ошибка синтаксиса
+var var13: String = "true"
+var var14: Char = '9'
+var var15: Int = 2048
+var var16: Long = 10000000000L
+var var17: Float = 5.75f
+var var19: Double = 1.414
+var var20: String = "Artificial Intelligence"
+var var21: Boolean = false
+var var22: Char = '@'
+var var23: Int = 1024
+var var24: Long = 1234567890123L
+var var25: Float = 10.01f
+var var26: Double = -273.15
+var var27: String = "SpaceX"
+var var28: Boolean = true
+“🤯” // ошибка синтаксиса
+var var29: String = "🤯"
+‘65535’ //ошибка синтаксиса
+var var30: String = "65535"
+var var31: Long = 72057594037927935L
+var var32: Float = 2.71828f
+var var33: Double = 101.0101
+var var34: String = "Quantum Computing"
+var var35: Boolean = false
+var var36: Char = 'x'
+var var37: Int = 314
+var var38: Long = 123456789123456789L
+var var39: Float = 6.626f
+var var40: Double = 0.007
+var var41: String = "Android Studio"
